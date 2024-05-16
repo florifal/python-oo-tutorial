d081917293c0cc524ba2673984e4c460aa3b438c
Florian Faltermeier <florifal@posteo.de>
2024-05-16 14:52:32 +0800

    Add service charge exercise

---
+++ part_09/04_scope_of_methods/service_charge.py
@@ -0,0 +1,35 @@
+class BankAccount:
+
+    def __init__(self, owner: str, account_number: str, balance: float) -> None:
+        self.__owner = owner
+        self.__account_number = account_number
+        self.__balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    def __service_charge(self):
+        self.__balance *= 0.99
+
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+        else:
+            raise ValueError("Deposit amount must be greater than zero.")
+
+    def withdraw(self, amount: float):
+        if amount > 0:
+            self.__balance -= amount
+            self.__service_charge()
+        else:
+            raise ValueError("Withdrawal amount must be greater than zero.")
+
+
+if __name__ == "__main__":
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
